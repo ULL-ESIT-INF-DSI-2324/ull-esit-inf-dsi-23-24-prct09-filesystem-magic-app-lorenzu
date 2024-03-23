e0f88241abbb39fc8ca66b75224c8698152b87e1
Lorenzo Roman <alu0101314990@ull.edu.es>
2024-03-23 23:41:42 +0000

    Metodos realizados

--- src/Carta.ts
+++ src/Carta.ts
@@ -49,9 +49,54 @@ interface Carta {
 function checkUserDirectory(usuario: string): string {
   const userDirectory = `./cartas/${usuario}/`;
   if(!fs.existsSync(userDirectory)){
-    fs.mkdirSync(userDirectory, {recursive: true});
+    //fs.mkdirSync(userDirectory, {recursive: true});
+    console.log(chalk.red(`No existe el usuario ${usuario}`))
   }
   return userDirectory;
+}
+
+function Mostrarporpantalla(data: string){
+  const carta = JSON.parse(data);
+  console.log('Contenido de la carta:');
+  console.log('ID:', carta.id);
+  console.log('Nombre:', carta.nombre);
+  console.log('Mana:', carta.mana);
+  switch (carta.color){
+    case "Blanco": 
+    console.log('Color:', chalk.bgWhite(carta.color));
+    break;
+    case "Negro": 
+    console.log('Color:', chalk.bgBlack(carta.color));
+    break;
+    case "Incoloro": 
+    console.log('Color:', chalk.bgGray(carta.color));
+    break;
+    case "Rojo": 
+    console.log('Color:', chalk.bgRed(carta.color));
+    break;
+    case "Verde": 
+    console.log('Color:', chalk.bgGreen(carta.color));
+    break;
+    case "Azul": 
+    console.log('Color:', chalk.bgBlue(carta.color));
+    break;
+    case "Multicolor": 
+    console.log('Color:', chalk.bgYellow(carta.color));
+    break;
+
+  }
+  console.log('Tipo:', carta.tipo);
+  console.log('Rareza:', carta.rareza);
+  console.log('Reglas:', carta.reglas);
+  if(carta.tipo === "Criatura"){
+  console.log('Fuerza:', carta.fuerza);
+  console.log('Resistencia:', carta.resistencia);
+  }
+  if(carta.tipo === "Planeswalker") {
+    console.log('Lealtad:', carta.lealtad);
+
+  }
+  console.log('Valor de mercado:', carta.valor_mercado);
 }
 
 
@@ -72,7 +117,57 @@ class ColecciondeCartas {
     }
 
     fs.writeFileSync(filePath, JSON.stringify(nuevaCarta));
-    console.log(`Carta agregada a la colección de ${usuario}`);
+    console.log(chalk.green(`Carta agregada a la colección de ${usuario}`));
+  }
+
+  public eliminarcarta(usuario: string, carta: Carta): void {
+    const userDirectory = checkUserDirectory(usuario);
+    const filePath = userDirectory + carta.id + '.json';
+    if(fs.existsSync(filePath)){
+      fs.unlinkSync(filePath);
+      console.log(chalk.green(`Carta eliminada de la colección de ${usuario}`))
+      return;
+    } else {
+      console.log(chalk.red(`La carta no existe en la colección de ${usuario}`))
+    }
+  }
+
+  public modificarcarta(usuario: string, carta: Carta): void {
+    const userDirectory = checkUserDirectory(usuario);
+    const filePath = userDirectory + carta.id + '.json';
+    if(fs.existsSync(filePath)){
+      fs.writeFileSync(filePath, JSON.stringify(carta));
+      console.log(chalk.green(`Carta modificada en la colección de ${usuario}`))
+      return;
+    } else {
+      console.log(chalk.red(`La carta no existe en la colección de ${usuario}`))
+    }
+  }
+
+  public mostrarcarta(usuario: string, id: number): void {
+    const userDirectory = checkUserDirectory(usuario);
+    const filePath = userDirectory + id + '.json';
+    if(fs.existsSync(filePath)){
+      const data = fs.readFileSync(filePath).toString();
+      Mostrarporpantalla(data);
+      return;
+    } else {
+      console.log(chalk.red(`La carta no existe en la colección de ${usuario}`))
+    }
+  }
+
+  public listarcartas(usuario: string): void{
+    const userDirectory = checkUserDirectory(usuario);
+    if(!fs.existsSync(userDirectory)){
+      console.log(chalk.red(`${usuario} no dispone de cartas`))
+    } else {
+      const cartas = fs.readdirSync(userDirectory);
+      cartas.forEach((archivo) => {
+        let filePath: string  = userDirectory + `${archivo}`;
+        const carta = fs.readFileSync(filePath).toString();
+        Mostrarporpantalla(carta);
+      })
+    }
   }
 }
 
@@ -89,4 +184,31 @@ const nuevaCarta: Carta = {
   valor_mercado: 10000
 }
 
-NuevaColeccion.agregarcarta("loren", nuevaCarta);
+const nuevaCarta2: Carta = {
+  id: 4,
+  nombre: "Rubiales",
+  mana: 30,
+  color: "Incoloro" as Color,
+  tipo: "Artefacto" as Tipo,
+  rareza: "Mítica" as Rareza,
+  reglas: "Un pikito <3",
+  valor_mercado: 10000
+}
+
+const nuevaCarta3: Carta = {
+  id: 3,
+  nombre: "Pepesin",
+  mana: 30,
+  color: "Incoloro" as Color,
+  tipo: "Artefacto" as Tipo,
+  rareza: "Mítica" as Rareza,
+  reglas: "Un pikito <3",
+  valor_mercado: 10000
+}
+
+NuevaColeccion.modificarcarta("loren", nuevaCarta);
+NuevaColeccion.mostrarcarta("loren", 3);
+NuevaColeccion.modificarcarta("pepe", nuevaCarta);
+NuevaColeccion.listarcartas("loren")
+
+
